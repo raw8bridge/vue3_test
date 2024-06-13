cdf2cb48e42318707a231748dfa917fa30d2da23
raw8bridge <achappy09tk@gmail.com>
2024-06-13 18:01:58 +0900

    p.252 list4-15 +alpha

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,17 +1,22 @@
 <template>
   <div className="alert alert-primary">
-    <h1>{{title}}</h1>
-    <p ref="msg">{{message}}</p>
+    <h2 class="card-title text-center">{{ title }}</h2>
+    <p>{{ message }}</p>
     <hr>
-    <p class="h5">val: {{ val }}</p>
     <div class="form-group text-left">
-      <label>* 2:</label>
-      <input type="number" v-model="a" class="form-control">
+      <label>Value1:</label>
+      <input type="number" v-model="val" class="form-control">
     </div>
     <div class="form-group text-left">
-      <label>^ 2:</label>
-      <input type="number" v-model="b" class="form-control">
+      <label>Value2:</label>
+      <input type="number" v-model="val2" class="form-control">
     </div>
+    <table class="bg-white table mt-4">
+      <tr><th>add:</th><td>{{ add }}</td></tr>
+      <tr><th>sub:</th><td>{{ sub }}</td></tr>
+      <tr><th>multiple:</th><td>{{ mult }}</td></tr>
+      <tr><th>divide:</th><td>{{ div }}</td></tr>
+    </table>
   </div>
 </template>
 
@@ -19,30 +24,44 @@
 export default {
   name: 'HelloWorld',
   props: {
-    title: String,
   },
   data() {
     return {
-      message:'算出プロパティの利用',
+      title: '',
+      message: '値の監視',
       val: 0,
+      val2: 1,
+      add: 0,
+      sub: 0,
+      mult: 0,
+      div: 0,
     }
   },
-  computed: {
-    a: {
-      get() {
-        return this.val * 2
-      },
-      set(value) {
-        this.val = Math.floor(value / 2)
-      },
+  watch: {
+    val(newValue, oldValue) {
+      console.log('val: ' + oldValue + ' -> ' + newValue)
+      this.calculate()
     },
-    b: {
-      get() {
-        return this.val * this.val
-      },
-      set(value) {
-        this.val = Math.floor(Math.sqrt(value))
-      },
+    val2(newValue, oldValue) {
+      console.log('val2: ' + oldValue + ' -> ' + newValue)
+      this.calculate()
+    }
+  },
+  methods: {
+    calculate() {
+      if(this.val != "" && this.val2 != "") {
+        var val = parseInt(this.val)
+        var val2 = parseInt(this.val2)
+        this.add = Math.floor(val + val2)
+        this.sub = Math.floor(val - val2)
+        this.mult = Math.floor(val * val2)
+        this.div = Math.floor(val / val2)
+      } else {
+        this.add = 0
+        this.sub = 0
+        this.mult = 0
+        this.div = 0
+      }
     },
   },
   created() {
