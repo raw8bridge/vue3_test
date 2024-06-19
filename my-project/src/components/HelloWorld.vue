f76695fdcedf588fedd8d43298445a614bb89580
raw8bridge <achappy09tk@gmail.com>
2024-06-19 16:46:10 +0900

    p.265 list4-20

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -3,20 +3,14 @@
     <h2 class="card-title text-center">{{ title }}</h2>
     <p>{{ message }}</p>
     <hr>
-    <div class="form-group text-left">
-      <label>Value1:</label>
-      <input type="number" v-model="val" class="form-control">
+    <div>
+      <input type="text" class="form-control"
+        v-on:keypress="type"
+        v-on:keydown.delete="clear"
+        v-on:keydown.space="space"
+        v-on:keydown.enter="enter"
+        v-on:keyup="form_clr">
     </div>
-    <div class="form-group text-left">
-      <label>Value2:</label>
-      <input type="number" v-model="val2" class="form-control">
-    </div>
-    <table class="bg-white table mt-4">
-      <tr><th>add:</th><td>{{ add }}</td></tr>
-      <tr><th>sub:</th><td>{{ sub }}</td></tr>
-      <tr><th>multiple:</th><td>{{ mult }}</td></tr>
-      <tr><th>divide:</th><td>{{ div }}</td></tr>
-    </table>
   </div>
 </template>
 
@@ -27,45 +21,28 @@ export default {
   },
   data() {
     return {
-      title: '',
-      message: '値の監視',
-      val: 0,
-      val2: 1,
-      add: 0,
-      sub: 0,
-      mult: 0,
-      div: 0,
-    }
-  },
-  watch: {
-    val(newValue, oldValue) {
-      console.log('val: ' + oldValue + ' -> ' + newValue)
-      this.calculate()
-    },
-    val2(newValue, oldValue) {
-      console.log('val2: ' + oldValue + ' -> ' + newValue)
-      this.calculate()
+      title: 'Event',
+      message: '',
     }
   },
   methods: {
-    calculate() {
-      if(this.val != "" && this.val2 != "") {
-        var val = parseInt(this.val)
-        var val2 = parseInt(this.val2)
-        this.add = Math.floor(val + val2)
-        this.sub = Math.floor(val - val2)
-        this.mult = Math.floor(val * val2)
-        this.div = Math.floor(val / val2)
-      } else {
-        this.add = 0
-        this.sub = 0
-        this.mult = 0
-        this.div = 0
-      }
+    type(event) {
+      if(event.key == 'Enter') { return }
+      this.message += event.key + ' '
+      event.target.value = ''
     },
-  },
-  created() {
-    this.val = 10
+    clear() {
+      this.message = ''
+    },
+    space() {
+      this.message += '_ '
+    },
+    enter(event) {
+      var res = this.message.split(' ').join('')
+      this.message = res.split('_').join(' ')
+      event.target.value = ''
+    },
+    form_clr(event) { event.target.value = '' },
   },
 }
 </script>
